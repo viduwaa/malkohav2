9c7851e3a4f50de10125ade6651c2805109fa357
Vidula Deneth <viduladenethsalwathura@gmail.com>
2024-04-24 11:50:45 +0530

    google login completed

---
+++ pages/config.php
@@ -0,0 +1,17 @@
+<?php
+
+$server = "localhost";
+$username = "root";
+$password = "";
+$dbname = "malkohav2";
+
+$conn = mysqli_connect($server, $username, $password, $dbname);
+
+if(!$conn){
+    die("Connection failed: ".mysqli_connect_error());
+}else{
+    echo "Connected successfully";
+}
+
+
+?>
---
+++ pages/login.php
@@ -0,0 +1,28 @@
+<?php
+    include ('config.php');
+    require_once (dirname(__DIR__).'../vendor/autoload.php' );
+
+    $client = new Google\Client();
+    $client->setAuthConfig(dirname(__DIR__).'/pages/client_secrets.json');
+    $client->addScope('profile');
+    $client->addScope('email');
+
+    if (! isset($_GET['code'])) {
+        $auth_url = $client->createAuthUrl();
+        echo "<a href='".filter_var($auth_url, FILTER_SANITIZE_URL)."'><button>Login With Google</button></a>";
+      } else {
+        $client->authenticate($_GET['code']);
+        $_SESSION['access_token'] = $client->getAccessToken();
+        $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/';
+        
+        $gauth = new Google\Service\Oauth2($client);
+        $google_info = $gauth -> userinfo -> get();
+
+        $email = $google_info ->email;
+        $name = $google_info -> name;
+
+        echo "Welcome".$name;
+      }
+    
+?>
+
