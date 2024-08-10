4c20eba4d821989e043620377155f6caf0ea6bca
maleecer <dushminmallesha10@gmail.com>
2024-08-10 23:11:08 +0530

    mac os software added

--- helpers/softwares.php
+++ helpers/softwares.php
@@ -21,6 +21,31 @@ $moduleSoftwares = [
         'img' => '../assets/soft-icons/audition.png',
         'link' => 'https://pixeldrain.com/u/uVh3VCL5'
     ]
+
+    $moduleSoftwaresmacos = [
+        [
+            'name' => 'Birdfont',
+            'img' => '../assets/soft-icons/birdfont.png',
+            'link' => 'https://birdfont.org/download/birdfont-6.7.1-free.dmg'
+        ],
+        
+        [
+            'name' => 'Adobe Photoshop 2023',
+            'img' => '../assets/soft-icons/photoshop.png',
+            'link' => 'https://igetintopc.com/adobe-photoshop-2023-free-download-macos/'
+        ],
+        [
+            'name' => 'Adobe Premiere Pro 2022',
+            'img' => '../assets/soft-icons/ppro.png',
+            'link' => 'https://igetintopc.com/adobe-premiere-pro-2022-free-download-macos/'
+        ],
+        [
+            'name' => 'Adobe Audition 2022',
+            'img' => '../assets/soft-icons/audition.png',
+            'link' => 'https://igetintopc.com/adobe-audition-2022-free-download-macos/'
+        ]
+    ]
+    
 ]
 
 ?>
